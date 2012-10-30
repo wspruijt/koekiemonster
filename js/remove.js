477f2402e22027f5ca8a41e64159cdcfaf01ee0a
Willem Spruijt <willem.spruijt@gmail.com>
2012-10-30 14:22:51 +0100

    Added sites

--- js/remove.js
+++ js/remove.js
@@ -4,15 +4,42 @@ $(function() {
 
   // Nu.nl
   if (url.indexOf("nu.nl") >= 0) {
-     $('span#sanoma-optin-close a').trigger('click');
+    $('span#sanoma-optin-close a').trigger('click');
   }
 
+  // Telegraaf
+  if (url.indexOf("telegraaf.nl") >= 0) {
+    setTimeout(function() { $('span#cookiepolicy-accept-cookies').trigger('click'); }, 500)
+  }
+
+  if (url.indexOf("ad.nl") >= 0) {
+    $('#cookiestatement').hide();
+  }
+
+  // NCR
+  if (url.indexOf("nrc.nl") >= 0) {
+    $('div.cookiemonster button').trigger('click'); // :)
+  }
+  
   // MSN
   if (url.indexOf("nl.msn.com") >= 0) {
     setTimeout(function(){ $('#cb1').hide(); }, 2000); 
   }
 
+  // Tweakers
+  if (url.indexOf("tweakers.net") >= 0) {
+    if($('div#cookieBar')) {
+      $('div#cookieBar input.fancyButton').trigger('click');
+    }
+  }
+
+  // Marktplaats
+  if (url.indexOf("marktplaats.nl") >= 0) {
+    $('div#cookie-opt-in-button').trigger('click');
+  }
 
 });
+
+
 
 
